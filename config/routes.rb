9f52ae68a4dac40be0fc74954915487c686cf88d
大西健 <ohnishiken5@gmail.com>
2024-02-11 19:05:12 +0900

    Task.11-2~4を実施

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Task.7-3 20240206 Ohnishi
   root to: 'articles#index'
-  #**************************
-  resources :sample_articles
-  # Task.7-2 articleのルーティングの実装
+  get '/mypage', to: 'mypage#show'
   resources :articles
-  #*********************************
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
