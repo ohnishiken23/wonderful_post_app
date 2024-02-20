0f1100306d995db565bd025054f0ea651e7b4adb
大西健 <ohnishiken5@gmail.com>
2024-02-20 23:04:37 +0900

    Task.15を実施

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,7 +1,7 @@
 class MypageController < ApplicationController
   def show
     articles = current_user.articles.all
-    articles = articles.where('articles.title LIKE(?)', "%#{params[:title]}")
+    articles = articles.where('articles.title LIKE(?)', "%#{params[:title]}%")
     @articles = articles.page params[:page]
   end
 end
